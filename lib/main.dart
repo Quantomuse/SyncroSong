48b0fa5fa8352e52ece932f5852ea4e68f811d2d
Tim Surkis <TSurkis@users.noreply.github.com>
2024-04-30 12:44:55 +0300

    Added api URL abstraction (#5)
    - Added support for local .env file.
    - Added api URLS + logic to read said api URLs.
    
    Co-authored-by: Tim Surkis <>

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:syncrosong/data/api/api.dart';
 import 'package:syncrosong/data/database/database.dart';
 import 'package:syncrosong/data/repos/songs/songs_repository.dart';
@@ -12,6 +13,7 @@ import 'pages/song_search/search_song_bloc.dart';
 import 'utility/logger.dart';
 
 void main() async {
+  await dotenv.load(fileName: "local_properties.env");
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: AppColors.statusBarColor,
